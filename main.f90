ad31c49fca772146387dc5255e164e3ac825ed56
scarpma <scarpma@gmail.com>
2020-01-17 18:15:24 +0100

    modified dt from 0.0001 to 0.001 and the program now converges.

--- main.f90
+++ main.f90
@@ -36,7 +36,7 @@ program ns_game
     ! INIZIALIZZO PARAMETRI
     LL = 1.0_sp
     h = LL/real(M,sp)
-    dt = 0.0001_sp
+    dt = 0.001_sp
     simtime = real(Niter,sp)*dt
     diff = 0.001_sp
     visc = 0.01_sp
